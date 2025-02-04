b51c35f21effbdb59e261fa35ae1cc03177e2f70
Gustin <gustin.mailhe@etu.univ-amu.fr>
2025-02-04 14:54:54 +0100

    Ajout des setters - Gustin-MAILHE

--- src/Bibliotheque/Livre.java
+++ src/Bibliotheque/Livre.java
@@ -9,12 +9,24 @@ public class Livre {
 		return quantity;
 	}
 	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
 	public String getName() {
 		return name;
 	}
 	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	public double getPrice() {
 		return price;
+	}
+	
+	public void setPrice(double price) {
+		this.price = price;
 	}
 	
 	public Livre() {
